965cd0486cd7dfba1dfaf7542420a67c1470b1fe
morfinPL <pl.morfin.pl@gmail.com>
2020-03-11 00:20:52 +0100

    Add selection by roulette.

--- apps/Task1.hs
+++ apps/Task1.hs
@@ -2,6 +2,7 @@ module Task1 where
 
 import qualified System.Random
 
+import qualified Evolutionary
 import qualified Objectives
 import qualified Utils
 
@@ -19,7 +20,17 @@ main = do
        let populationScaledXPoints = map (Utils.scale (-7.5, 7.5) 0) populationPoints
        let populationScaledPoints = map (Utils.scale (-7.5, 7.5) 1) populationScaledXPoints
        let computedPoints = map (Utils.compute Objectives.firstFunction) populationScaledPoints
-       if populationSize * numberOfFeatures <= 100
-           then putStrLn ("Initial population: " ++ show population ++ "\n" ++ show populationPoints ++ "\n" ++ show populationScaledXPoints ++ "\n" ++ show populationScaledPoints)
-       else putStrLn "Initial population is too big to be displayed."
        Utils.plot "Initial population" Objectives.firstFunctionString 50 (-25) (-7.5, 7.5) (-7.5, 7.5) computedPoints
+       let probabilities = Evolutionary.roulette computedPoints
+       if populationSize <= 100
+           then putStrLn ("Initial population:\n" ++ show computedPoints ++ "\n" ++ show probabilities)
+       else putStrLn "Population is too big to be displayed."
+       let newPopulation = Evolutionary.generateNewPopulationByRoulette generator population probabilities
+       let newPopulationPoints = map (Utils.individualToPoint 2) newPopulation
+       let newPopulationScaledXPoints = map (Utils.scale (-7.5, 7.5) 0) newPopulationPoints
+       let newPopulationScaledPoints = map (Utils.scale (-7.5, 7.5) 1) newPopulationScaledXPoints
+       let newComputedPoints = map (Utils.compute Objectives.firstFunction) newPopulationScaledPoints
+       Utils.plot "New population" Objectives.firstFunctionString 50 (-25) (-7.5, 7.5) (-7.5, 7.5) newComputedPoints
+       if populationSize <= 100
+           then putStrLn ("New population: \n" ++ show newComputedPoints ++ "\n")
+       else putStrLn "Population is too big to be displayed."
