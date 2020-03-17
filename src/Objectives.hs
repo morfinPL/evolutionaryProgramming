e2ecfd6cc75c385d2c1dffb248de7110f53707c3
morfinPL <pl.morfin.pl@gmail.com>
2020-03-17 23:06:43 +0100

    Add new objective functions.

--- src/Objectives.hs
+++ src/Objectives.hs
@@ -13,6 +13,10 @@ data ObjectiveFunction = ObjectiveFunction
 
 parseObjectiveFunction :: String -> Objectives.ObjectiveFunction
 parseObjectiveFunction s = case s of
+                            "seventh" -> Objectives.seventhFunction
+                            "sixth" -> Objectives.sixthFunction
+                            "fifth" -> Objectives.fifthFunction
+                            "fourth" -> Objectives.fourthFunction
                             "third" -> Objectives.thirdFunction
                             "second"   -> Objectives.secondFunction
                             _ -> Objectives.firstFunction
@@ -35,6 +39,7 @@ secondFunction = ObjectiveFunction (\x y -> -20 * exp(-0.2 * sqrt(0.5 * (x^2 + y
                                     (-5, 5)
                                     (-5, 5)
 
+
 thirdFunction :: ObjectiveFunction
 thirdFunction = ObjectiveFunction (\x y -> 0.5 + (sin (x^2 - y^2)^2 -0.5)/((1+ 0.001 * (x^2 + y^2))^2))
                                   "0.5 + ((sin(x**2 - y**2))**2 -0.5)/((1+ 0.001 * (x**2 + y**2))**2)"
@@ -42,3 +47,39 @@ thirdFunction = ObjectiveFunction (\x y -> 0.5 + (sin (x^2 - y^2)^2 -0.5)/((1+ 0
                                   0
                                   (-100, 100)
                                   (-100, 100)
+
+
+fourthFunction :: ObjectiveFunction
+fourthFunction = ObjectiveFunction (\x y -> sin (3 * pi * x)^2 + (x -1)^2 * (1 + sin (3 * pi * y)^2) + (y -1)^2 * (1 + sin (2 * pi * y)^2))
+                                  "(sin(3 * pi * x))**2 + (x -1)**2 * (1 + (sin(3 * pi * y))**2) + (y -1)**2 * (1 + (sin(2 * pi * y))**2)"
+                                  100
+                                  0
+                                  (-10, 10)
+                                  (-10, 10)
+
+
+fifthFunction :: ObjectiveFunction
+fifthFunction = ObjectiveFunction (\x y -> 100 * sqrt (abs (y -0.01 * x^2)) + 0.01 * abs (x + 10))
+                                  "100 * sqrt(abs(y -0.01 * x**2)) + 0.01 * abs(x + 10)"
+                                  100
+                                  0
+                                  (-15, -5)
+                                  (-3, 3)
+
+
+sixthFunction :: ObjectiveFunction
+sixthFunction = ObjectiveFunction (\x y -> (-(y +47)) * sin (sqrt (abs (0.5 * x + y + 47))) - x * sin (sqrt (abs (x -y -47))))
+                                  "-(y +47) * sin(sqrt(abs(0.5 * x + y + 47))) - x * sin(sqrt(abs(x -y -47)))"
+                                  100
+                                  (-1000)
+                                  (-512, 512)
+                                  (-512, 512)
+
+
+seventhFunction :: ObjectiveFunction
+seventhFunction = ObjectiveFunction (\x y -> 20 + x^2 - 10 * cos (2 * pi * x) + y^2 - 10 * cos (2 * pi * y))
+                                  "20 + x**2 - 10 * cos(2 * pi * x) + y**2 - 10 * cos(2 * pi * y)"
+                                  100
+                                  0
+                                  (-5, 5)
+                                  (-5, 5)
