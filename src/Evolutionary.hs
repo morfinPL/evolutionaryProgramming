96a480eb1be1d5039dd4dd2ef414db0713c82ce8
Filip Pietrusiak <filip.pietrusiak@gmail.com>
2020-03-29 23:35:18 +0200

    Move function generatePopulation from Utils to evolutionary.

--- src/Evolutionary.hs
+++ src/Evolutionary.hs
@@ -1,5 +1,29 @@
 module Evolutionary where
 
+import qualified Data.List.Split                ( chunksOf )
+import qualified System.Random                  ( RandomGen
+                                                , randoms
+                                                )
+
+generatePopulation
+  :: System.Random.RandomGen g
+  => g
+  -> Int
+  -> Int
+  -> Int
+  -> ([Bool] -> [Bool])
+  -> [[[Bool]]]
+generatePopulation generator populationSize dimensions numberOfFeaturesPerDimension coding
+  = map
+    (map coding . Data.List.Split.chunksOf numberOfFeaturesPerDimension)
+    (Data.List.Split.chunksOf
+      (dimensions * numberOfFeaturesPerDimension)
+      (take (dimensions * numberOfFeaturesPerDimension * populationSize)
+            (System.Random.randoms generator :: [Bool])
+      )
+    )
+  where booleaner x = x == 1
+
 nextGeneration
   :: ([Bool] -> [Bool])
   -> ([[[Bool]]] -> [[Double]] -> [[[Bool]]])
