88c90cad1b252c1ae10ea3171e3259e9ac2e275d
Filip Pietrusiak <filip.pietrusiak@gmail.com>
2020-03-29 23:02:08 +0200

    Enable setting computePoints from outside.

--- src/Evolutionary.hs
+++ src/Evolutionary.hs
@@ -1,27 +1,18 @@
 module Evolutionary where
 
-import qualified Utils
-
-
 nextGeneration
   :: ([Bool] -> [Bool])
   -> ([[[Bool]]] -> [[Double]] -> [[[Bool]]])
   -> ([[[Bool]]] -> [[[Bool]]])
   -> ([[[Bool]]] -> [[[Bool]]])
-  -> (Double, Double)
-  -> (Double, Double)
-  -> (Double -> Double -> Double)
+  -> ([[[Bool]]] -> [[Double]])
   -> ([[[Bool]]], [[Double]])
   -> ([[[Bool]]], [[Double]])
-nextGeneration decoding selection mutation crossover rangeX rangeY objectiveFunction population
-  = do
+nextGeneration decoding selection mutation crossover computePoints population =
+  do
     let oldPopulation = fst population
     let oldPoints     = snd population
     let parents       = selection oldPopulation oldPoints
     let newPopulation = crossover (mutation parents)
-    let newPoints = Utils.computePoints objectiveFunction
-                                        rangeX
-                                        rangeY
-                                        decoding
-                                        newPopulation
+    let newPoints     = computePoints newPopulation
     (newPopulation, newPoints)
